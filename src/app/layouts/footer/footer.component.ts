13bf92ad24a265e46563fde3e4e79492541b66f8
IonellaP <ionelapb27@gmail.com>
2025-03-26 13:54:50 +0200

    created and added footer

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   standalone: true,
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
